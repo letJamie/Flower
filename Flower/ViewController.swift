b8cfb9795f69a1507d57d10595deb5009ba44aa3
letJamie <stillbut@naver.com>
2020-08-19 16:14:29 +0900

    Get classification from model

--- Flower/ViewController.swift
+++ Flower/ViewController.swift
@@ -1,20 +1,77 @@
 //
 //  ViewController.swift
-//  Flower
+//  Flowers
 //
-//  Created by Jamie on 2020/08/19.
+//  Created by Jamie on 2020/08/18.
 //  Copyright © 2020 Jamie. All rights reserved.
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var photoView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        
+        
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            
+            photoView.image = userPickedImage
+            
+            guard let convertedCiImage = CIImage(image: userPickedImage) else {
+                fatalError("can't convert to CIImage")
+            }
+            
+            detect(image: convertedCiImage)
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError()
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title = classification?.identifier
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
 
+        } catch {
+            print("error")
+        }
+        
+    }
+    
+    
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
